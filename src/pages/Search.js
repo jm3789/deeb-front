1a3b3477e4b27c7076c2dffd3d2de5847a667433
jm3789 <jm37890@gmail.com>
2024-04-07 16:58:11 +0900

    style: card 사용해 책 정보 표시

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
-import { TextField, Button } from "@mui/material";
+import {
+	TextField,
+	Button,
+	Typography,
+	Card,
+	CardContent,
+	CardMedia,
+	Grid,
+} from "@mui/material";
 
 const Search = () => {
-	const [title, setTitle] = useState();
-	const [result, setResult] = useState("결과가 없습니다.");
+	const [title, setTitle] = useState("");
+	const [results, setResults] = useState([]);
+
 	const searchBook = async (query) => {
 		try {
 			const res = await axios.get("/v1/search/book.json", {
@@ -14,29 +23,76 @@ const Search = () => {
 					"X-Naver-Client-Secret": process.env.REACT_APP_CLIENT_SECRET,
 				},
 			});
-			let result = [];
-			res.data.items.forEach((item) => result.push(item.title));
-			setResult(JSON.stringify(result));
+			setResults(res.data.items);
 		} catch (err) {
 			console.error(err);
 		}
 	};
 
+	const truncateDescription = (description) => {
+		if (description.length > 200) {
+			return description.substring(0, 200) + "...";
+		}
+		return description;
+	};
+
 	return (
 		<div>
 			<h1>책 검색</h1>
-			<div>
+			<div style={{ marginBottom: "20px" }}>
 				<TextField
 					label="책 제목"
 					variant="standard"
-					onChange={(e) => {
-						setTitle(e.target.value);
-					}}
+					onChange={(e) => setTitle(e.target.value)}
 				/>
 				<Button variant="contained" onClick={() => searchBook(title)}>
 					검색
 				</Button>
-				<div>{result}</div>
+			</div>
+			<div>
+				{results.map((item, index) => (
+					<Card
+						key={index}
+						variant="outlined"
+						style={{ marginBottom: "0px", display: "flex" }}
+					>
+						<CardMedia
+							component="img"
+							image={item.image}
+							alt={item.title}
+							style={{
+								objectFit: "contain",
+								width: "200px",
+								height: "200px",
+							}}
+						/>
+						<CardContent style={{ flexGrow: 1 }}>
+							<Grid container spacing={2}>
+								<Grid item xs={12}>
+									<Typography gutterBottom variant="h5" component="div">
+										{item.title}
+									</Typography>
+								</Grid>
+								<Grid item xs={12}>
+									<Typography variant="body2" color="text.secondary">
+										저자: {item.author}
+										<br />
+										출판사: {item.publisher}
+										<br />
+										출판일: {item.pubdate.substring(0, 4)}/
+										{item.pubdate.substring(4, 6)}/
+										{item.pubdate.substring(6, 8)}
+									</Typography>
+								</Grid>
+								<Grid item xs={12}>
+									<Typography variant="body2" color="text.secondary">
+										{truncateDescription(item.description)}
+									</Typography>
+								</Grid>
+							</Grid>
+						</CardContent>
+					</Card>
+				))}
 			</div>
 		</div>
 	);
